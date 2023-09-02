69c771cfbdfb59db6b1863c9b48806213d37b801
yanghojoon <yanghojoon8487@gmail.com>
2023-09-02 15:26:48 +0900

    chore: 스토리보드 제거

---
+++ Movie+RIBs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie+RIBs
+//
+//  Created by 양호준 on 2023/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
