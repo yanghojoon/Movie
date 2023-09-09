f6ed91f4d6b2743cbe96714b2ccbe511b0bccc4b
yanghojoon <yanghojoon8487@gmail.com>
2023-09-09 17:01:33 +0900

    feat: 탭바 아이템 추가

--- Movie+RIBs/List/ListViewController.swift
+++ Movie+RIBs/List/ListViewController.swift
@@ -17,4 +17,25 @@ protocol ListPresentableListener: AnyObject {
 final class ListViewController: UIViewController, ListPresentable, ListViewControllable {
 
     weak var listener: ListPresentableListener?
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setupViews()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupViews() {
+        tabBarItem = UITabBarItem(
+            title: "List",
+            image: UIImage(systemName: "list.bullet.rectangle"),
+            selectedImage: UIImage(systemName: "list.bullet.rectangle.fill")
+        )
+
+        view.backgroundColor = .systemBlue
+    }
 }
